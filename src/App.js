5fcddbe3c244dab82278dbd236f96bdf16f5aaa8
Rich Korneisel <korneisel@cfu.net>
2021-02-18 16:56:21 -0600

    Dad Jokes Complete

--- src/App.js
+++ src/App.js
@@ -1,18 +1,44 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
+// import DadJokes from './DadJokes'
 
 class App extends Component {
+  constructor() {   // always add constructor and super
+    super()
+
+    this.state={
+      jokes: []         //api data usually in array
+    }
+  }
+  componentDidMount = () => {
+    axios.get('https://icanhazdadjoke.com/', {
+      headers: {
+        Accept: 'application/json'
+      }
+    })
+    .then(response => {
+      this.setState({
+        jokes: response.data
+      })
+    })
+  }
+  
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">Dad Jokes</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        
+        <div className="newJoke">
+        {this.state.jokes.joke}
+        </div>
+        
+        <div className="button">
+        <button onClick={this.componentDidMount}>New Joke</button>
+        </div>
       </div>
     );
   }
---
+++ src/DadJokes.js
@@ -0,0 +1,18 @@
+// import React, { Component } from "react";
+
+// class DadJokes extends Component {
+//   render() {
+//     let list = [];
+//     const DadJokesList = this.props.jokes.map((jokes, index) => 
+//         list.push(<DadJokes name={jokes.name} key={index}/>)
+//     )
+//     return (
+//         <div>
+//             <h1>Random Dad Joke</h1> 
+//             {list}   
+//         </div>
+//     ) 
+//   }
+// }
+
+// export default DadJokes;
